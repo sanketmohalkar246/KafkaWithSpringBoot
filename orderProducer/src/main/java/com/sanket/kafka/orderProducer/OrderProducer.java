93a14c094573733c5581ca7e6ae10f52d85ccd87
Sanket Mohalkar <sanketmohalkar246@gmail.com>
2025-03-29 01:12:19 +0530

    #Sync call

--- orderProducer/src/main/java/com/sanket/kafka/orderProducer/OrderProducer.java
+++ orderProducer/src/main/java/com/sanket/kafka/orderProducer/OrderProducer.java
@@ -5,6 +5,7 @@ import java.util.Properties;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.RecordMetadata;
 import org.apache.kafka.common.serialization.IntegerSerializer;
 import org.apache.kafka.common.serialization.StringSerializer;
 
@@ -17,16 +18,19 @@ public class OrderProducer {
 		props.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
 		props.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, IntegerSerializer.class.getName());
 
-		KafkaProducer<String, Integer> Producer = new KafkaProducer<String, Integer>(props);
+		KafkaProducer<String, Integer> producer = new KafkaProducer<String, Integer>(props);
 		ProducerRecord<String, Integer> record = new ProducerRecord<String, Integer>("OrderTopic", "Mac book pro", 10);
 
 		try {
-			Producer.send(record);
+			RecordMetadata recordMetadata =  producer.send(record).get();
+			//Sync call
+			System.out.println(recordMetadata.partition());
+			System.out.println(recordMetadata.offset());
 			System.out.println("Message Sent!");
 		} catch (Exception e) {
 			e.printStackTrace();
 		} finally {
-			Producer.close();
+			producer.close();
 		}
 	}
 
