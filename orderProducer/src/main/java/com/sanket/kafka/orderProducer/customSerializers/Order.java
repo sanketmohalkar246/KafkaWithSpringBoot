51d42248f11d94632d55338d102ed499a3ddf567
Sanket Mohalkar <sanketmohalkar246@gmail.com>
2025-03-29 17:42:04 +0530

    Serializer added

---
+++ orderProducer/src/main/java/com/sanket/kafka/orderProducer/customSerializers/Order.java
@@ -0,0 +1,30 @@
+package com.sanket.kafka.orderProducer.customSerializers;
+
+public class Order {
+	
+	private String customerName;
+	private String product;
+	private int quantity;
+	
+	public String getCustomerName() {
+		return customerName;
+	}
+	public void setCustomerName(String customerName) {
+		this.customerName = customerName;
+	}
+	public String getProduct() {
+		return product;
+	}
+	public void setProduct(String product) {
+		this.product = product;
+	}
+	public int getQuantity() {
+		return quantity;
+	}
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+	
+	
+
+}
