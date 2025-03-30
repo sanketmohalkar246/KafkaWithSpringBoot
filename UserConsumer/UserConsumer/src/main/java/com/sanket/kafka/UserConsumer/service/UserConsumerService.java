faec863dd7081e691c44798792b56368335c4da8
Sanket Mohalkar <sanketmohalkar246@gmail.com>
2025-03-31 00:42:54 +0530

    Created a consumer

---
+++ UserConsumer/UserConsumer/src/main/java/com/sanket/kafka/UserConsumer/service/UserConsumerService.java
@@ -0,0 +1,14 @@
+package com.sanket.kafka.UserConsumer.service;
+
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserConsumerService {
+	
+	@KafkaListener(topics = {"user-topic"})
+	public void consumeUserData(Integer age) {
+		
+	}
+
+}
